5525d243871edad15f98b78b50271696a320cf11
Antonia <antoniarata@yahoo.com>
2024-12-02 13:03:43 +0200

    home page + book details

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import './Home.css';
 
 export default function Home () {
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+  const [books, setBooks] = useState([]);
+  const [displayedBooks, setDisplayedBooks] = useState([]);
 
    useEffect (() => {
       fetch( `${backendUrl}/token`, {
@@ -25,9 +29,43 @@ export default function Home () {
       });
 },[backendUrl]);
 
+useEffect(() => {
+  fetch(`${backendUrl}/getBooks`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+  },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(books => {
+      setBooks(books);
+      setDisplayedBooks(books.slice(0, 50));
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }, [backendUrl]);
+
+  const onClickHandle = (id) => {
+    window.location.href = `/book/${id}`;
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+      <ul className="books-list">
+        {displayedBooks.map(book => (
+          <li key={book.bookId} onClick={() => onClickHandle(book.bookId )}>
+            <img src={book.coverPictureUrl} alt={book.title} />
+            <div className="info">
+              <h4>{book.title}</h4>
+              <p>{book.author}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
   );
 }
