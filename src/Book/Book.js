1d7eed758ff258ba1f594e72c4f472a52f35166e
Antonia <antoniarata@yahoo.com>
2025-01-07 13:23:05 +0200

    add book to shelf functionality for button

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './Book.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Book() {
     const backendUrl = process.env.REACT_APP_BACKEND_URL;
     const [book, setBook] = useState({});
-    const id = parseInt(window.location.pathname.split('/')[2]);
+    const bookId = parseInt(window.location.pathname.split('/')[2]);
+    const userId = sessionStorage.getItem('userId') || sessionStorage.getItem('userIdGoogle');
 
     useEffect(() => {
-        fetch(`${backendUrl}/book?id=${id}`,{
+        fetch(`${backendUrl}/book?id=${bookId}`,{
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
@@ -18,12 +21,26 @@ export default function Book() {
                 }
                 return response.json();
         }).then(book => {
-            console.log(book);
             setBook(book);
         }).catch(error => {
             console.error('Error:', error);
         });
-    }, [backendUrl, id]);
+    }, [backendUrl, bookId]);
+
+    const addBookToShelf = () => {
+        fetch(`${backendUrl}/bookshelves/${userId}/myFutureReadings/add-book/${bookId}`, {
+          method: 'POST'
+        })
+          .then(() => {
+            toast.success('Book successfully added to your shelf!', {
+                position: "bottom-right",
+                autoClose: 3000, 
+                hideProgressBar: true,
+                pauseOnHover: true,
+              });
+          })
+          .catch((err) => console.error('Error adding book:', err));
+      };
 
     return (
         <div className='book-container'>
@@ -44,11 +61,14 @@ export default function Book() {
                     <div className="btn-container">
                         <button className="btn review-btn">Review Book</button>
                         <div className="vertical-border"></div>
-                        <button className="btn add-btn">Add to Shelf</button>
+                        <button className="btn add-btn" 
+                        onClick={() => addBookToShelf()}
+                        >Add to Shelf</button>
                     </div>
                     <div className="border bottom-border"></div>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 }
